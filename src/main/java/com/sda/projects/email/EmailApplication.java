5ea74350ed0cd9333fe5e183932b66e18a5c695a
Edvinas Prokofijovas <p998ffp@fspa.myntet.se>
2020-04-03 19:40:50 +0300

    updated email app

--- src/main/java/com/sda/projects/email/EmailApplication.java
+++ src/main/java/com/sda/projects/email/EmailApplication.java
@@ -4,37 +4,51 @@ import java.util.Scanner;
 
 public class EmailApplication {
 
-    public static void main(String[] args) {
-        Email[] emails = new Email[5];
+	public static void main(String[] args) {
+		Email[] emails = new Email[5];
 
-        initiateApplication(emails);
-        }
+		initiateApplication(emails);
+	}
 
-    private static void initiateApplication(Email[] emails) {
-        System.out.println("Tolimesni veiksmai?\n1.Prideti nauja vartotoja.\n2.Istrinti paskutini vartotoja.\n0.Baigti darba.");
-        Scanner scanner = new Scanner(System.in);
-        int decision = scanner.nextInt();
+	private static void initiateApplication(Email[] emails) {
 
-        while(decision != 0) {
-            if (decision == 2) {
-                System.out.println("El. pasto dezute " + emails[0].getEmail() + " buvo sekmingai istrinta");
-                emails[0] = null;
-            } else if (decision == 1) {
-                System.out.println("Iveskite varda: ");
-                String firstName = scanner.next();
-                System.out.println("Iveskite pavarde: ");
-                String lastName = scanner.next();
+		int decision = 5;
 
-                Email secondEmail = new Email(firstName, lastName);
+		while (decision != 0) {
 
-                for (int i = 0; i < emails.length; i++) {
-                    if (emails[i] == null) {
-                        emails[i] = secondEmail;
-                    }
-                }
+			System.out.println(
+					"Galimi veiksmai\n1.Prideti nauja vartotoja.\n2.Istrinti seniauiai ivesta vartotoja.\n0.Baigti darba.");
+			Scanner scanner = new Scanner(System.in);
+			decision = scanner.nextInt();
+			if (decision == 2) {
+				System.out.println("El. pasto dezute " + emails[0].getEmail() + " buvo sekmingai istrinta");
+				emails[0] = null;
+			} else if (decision == 1) {
+				System.out.println("Iveskite varda: ");
+				String firstName = scanner.next();
+				System.out.println("Iveskite pavarde: ");
+				String lastName = scanner.next();
 
-                decision = 0;
-            }
-        }
-    }
+				Email email = new Email(firstName, lastName);
+
+				for (int i = 0; i < emails.length; i++) {
+					if (emails[i] == null) {
+						emails[i] = email;
+						break;
+					}
+				}
+				System.out.println("************************************");
+				System.out.println("Egzistuojantys adresai sistemoje: \n");
+				for (int i = 0; i < emails.length; i++) {
+					if(emails[i] != null) {
+						System.out.println("El. pasto adresas: " + emails[i].getEmail() + " Dezutes talpa: "
+								+ emails[i].getMailboxCapacity() + "\n");
+					}
+					
+				}
+				System.out.println("************************************");
+
+			}
+		}
+	}
 }
